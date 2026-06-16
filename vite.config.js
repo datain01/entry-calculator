50d9283d587b8f40e4e38d0db119f45fb587edcc
datain01 <baksoeun01@gmail.com>
2026-06-16 19:59:15 +0900

    base 수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/entry-calculator/', // 💡 여기에 본인의 GitHub 저장소 이름을 정확히 적어주세요!
+  base: '/', 
 })
